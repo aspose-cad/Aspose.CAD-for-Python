889a3e1d0b44fb98ca361b3dd00c985f2a68e7a9
Vitalii Kornyliuk <vitalii.kornyliuk@aspose.com>
2024-04-26 14:09:02 +0300

    remove word examples and added cad conversion examples

---
+++ examples/conversion/convert-3ds-to-fbx.py
@@ -0,0 +1,15 @@
+import aspose.cad as cad
+
+image = cad.Image.load("file.3ds")
+
+cadRasterizationOptions = cad.imageoptions.CadRasterizationOptions()
+cadRasterizationOptions.page_height = 800.5
+cadRasterizationOptions.page_width = 800.5
+cadRasterizationOptions.zoom = 1.5
+cadRasterizationOptions.layers = "Layer"
+cadRasterizationOptions.background_color = Color.green
+
+options = FbxOptions()
+options.vector_rasterization_options = cadRasterizationOptions
+
+image.save("result.fbx", options)
---
+++ examples/conversion/convert-dwg-to-jpeg.py
@@ -0,0 +1,15 @@
+import aspose.cad as cad
+
+image = cad.Image.load("file.dwg")
+
+cadRasterizationOptions = cad.imageoptions.CadRasterizationOptions()
+cadRasterizationOptions.page_height = 800.5
+cadRasterizationOptions.page_width = 800.5
+cadRasterizationOptions.zoom = 1.5
+cadRasterizationOptions.layers = "Layer"
+cadRasterizationOptions.background_color = Color.green
+
+options = JpegOptions()
+options.vector_rasterization_options = cadRasterizationOptions
+
+image.save("result.jpeg", options)
